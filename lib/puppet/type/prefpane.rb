bbb22529254705cfeae348d7f31b178097f920c0
Andre Arko <andre@arko.net>
2013-11-22 23:39:56 -0800

    default to ensure true

--- lib/puppet/type/prefpane.rb
+++ lib/puppet/type/prefpane.rb
@@ -1,7 +1,17 @@
 Puppet::Type.newtype(:prefpane) do
   @doc = "Manages OS X System Preference Panes"
 
-  ensurable
+  ensurable do
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    defaultto(:present)
+  end
 
   newparam(:name, :namevar => true) do
     desc "The name of the preference pane"
